5a09d7e402c24a57eae40f37500d2eeefedc622d
cbq <cbqwill@163.com>
2021-01-24 01:05:01 +0800

    分支2方法第4次修改

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -1,8 +1,6 @@
 public class test {
     public static void main(String[] args) {
         System.out.println("555");
-        System.out.println("66666");
-        System.out.println("fenzhi777");
-        System.out.println("22222222222222222222222222");
+        System.out.println("222");
     }
 }
--- src/main/java/test22222222222.java
+++ src/main/java/test22222222222.java
@@ -1,6 +1,6 @@
 public class test22222222222 {
     public static void main(String[] args) {
-        System.out.println("2222222");
-        System.out.println("33333333333333333");
+        System.out.println("222");
+
     }
 }
