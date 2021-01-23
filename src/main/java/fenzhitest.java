fb772d51695146cbfadf3dfd81449f1f5e20b350
cbq <cbqwill@163.com>
2021-01-23 20:08:40 +0800

    分支第一次提交

---
+++ src/main/java/fenzhitest.java
@@ -0,0 +1,5 @@
+public class fenzhitest {
+    public static void main(String[] args) {
+        System.out.println("fezhi1111");
+    }
+}
--- src/main/java/test.java
+++ src/main/java/test.java
@@ -2,5 +2,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("555");
         System.out.println("66666");
+        System.out.println("fenzhi777");
     }
 }
